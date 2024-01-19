effd7d75569173ff47324edbf7412e09138fad2b
AnnRazareniva <145858547+AnnRazareniva@users.noreply.github.com>
2024-01-19 11:26:03 +0700

    Create Client.cpp

---
+++ Client.cpp
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char message[]="Hi, it's me, Client!";
+    int sock;
+    struct sockaddr_in addr;
+
+    sock=socket(AF_INET, SOCK_STREAM, 0);
+    if (sock<0)
+    {
+        perror("socket error");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect error");
+        exit(2);
+    }
+
+    send(sock, message, sizeof(message), 0);
+
+    close(sock);
+    return 0;
+}
